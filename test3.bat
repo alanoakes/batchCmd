cd142b1a92ddc69bf051d213f859f77d54a21cd9
Parker Oakes <parker.oakes@clkmm.com>
2020-12-08 22:26:41 -0600

    finished test3

--- test3.bat
+++ test3.bat
@@ -1,3 +1,15 @@
 @echo off
-title Start & Color Cmd
+rem Start & Color Cmd
+start http://192.168.1.254/cgi-bin/home.ha
+start https://mail.google.com/mail/u/0/#inbox
+start https://taskwarrior.org/
 
+rem color cmd uses background first and foreground second
+color 0a
+echo hello
+pause
+color 05
+echo hello
+pause
+color fe
+pause
